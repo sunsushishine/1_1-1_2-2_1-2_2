3bf2a5b5ba054581b87a33873656e0a9f00d8ad5
сырочек ✨ <57293178+sunsushishine@users.noreply.github.com>
2023-03-28 10:57:45 +0300

    Update 2_1.py

--- Задание 2_1/2_1.py
+++ Задание 2_1/2_1.py
@@ -1,8 +1,10 @@
 import urllib.request
+
 lst = list()
+
 with urllib.request.urlopen('http://dfedorov.spb.ru/python3/src/romeo.txt') as webpage:
         for i in webpage:
-            i = i.decode('utf-8')
+            i = i.decode(encoding='utf-8')
             i = i.strip()
             for word in i.split():
                 lst.append(word)
@@ -15,6 +17,4 @@ for word in lst:
         word_count[word] = 1
 
 for word, count in word_count.items():
-    print(f"{word} --> {count}")
-
-#print(lst)    
+    print(f"{word} -> {count}")
