b9ac2388bd78c92b4e2abe56d1161057b92eaace
сырочек ✨ <57293178+sunsushishine@users.noreply.github.com>
2023-04-04 15:06:36 +0300

    Update 2_2.py

--- Задание 2_2/2_2.py
+++ Задание 2_2/2_2.py
@@ -1,13 +1,15 @@
-import csv
+import csv as file
 
-arr={}
+list={}
 temp_name='Россия'
 temp_max=0
 str=0
 i=1
 
+print('Первые 10 регионов')
+
 with open('opendata.csv') as csvfile:
-    credit_reader=csv.reader(csvfile, delimiter=',')
+    credit_reader=file.reader(csvfile, delimiter=',')
     for row in credit_reader:
         if row[0]=='Количество заявок на потребительские кредиты':
             lst = row[2].split('-')
@@ -15,15 +17,16 @@ with open('opendata.csv') as csvfile:
                 if temp_name==row[1]:
                     temp_max+=int(row[3])
                 else:
-                    arr[temp_name]=temp_max
+                    list[temp_name]=temp_max
                     temp_max=0
                     temp_name=row[1]
                     temp_max+=int(row[3])
 
-for word, count in sorted(arr.items(), key=lambda item: item[1], reverse=True):
+for word, count in sorted(list.items(), key=lambda item: item[1], reverse=True):
     print(f"{i} {word} -> {count}")
     i+=1
     str+=1
     if str==10:
         break
-print('Регионом с максимальным количеством заявок на потребительские кредиты в 2017 году является:', max(arr, key=arr.get))
+
+print('Регионом с максимальным количеством заявок на потребительские кредиты в 2017 году является:', max(list, key=list.get))
