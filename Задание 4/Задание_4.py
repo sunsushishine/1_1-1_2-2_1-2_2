c7ff2e6c693f660c197629902c633234dc2edaa0
сырочек ✨ <57293178+sunsushishine@users.noreply.github.com>
2023-04-06 10:20:47 +0300

    Add files via upload

---
+++ Задание 4/Задание_4.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+A = np.array([[1, -1, 1], [2, 1, 1], [1, 1, 1]])
+
+B = np.array([3, 11, 8])
+
+X = np.linalg.solve(A, B)
+
+print("Решение системы линейных уравнений 1:")
+print(X)
+
+A = np.array([[1, 2, -1], [2, -3, 1], [4, 1, -1]])
+
+B = np.array([7, 3, 16])
+
+try:
+    X = np.linalg.solve(A, B)
+    print("Решение системы линейных уравнений 2:")
+    print(X)
+except:
+    print("Нет решения")
