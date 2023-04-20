877563e67a762ecaf25270978794a9876e3fa237
сырочек ✨ <57293178+sunsushishine@users.noreply.github.com>
2023-04-20 10:40:18 +0300

    Update 5.py

--- Задание 5/5.py
+++ Задание 5/5.py
@@ -1,62 +1,42 @@
 import random
 
-print("Добро пожаловать в игру 'Камень, ножницы, бумага'!")
+CountD = 0
+CountL = 0
+CountW = 0
+l = ['k','n','b'] 
 
-end_game = True
-
-variants = ['камень', 'ножницы', 'бумага']
-
-while end_game == True:
-    # Запрос выбора игрока
-    print("Выберите вариант:")
-    for index, variant in enumerate(variants):
-        print(f"{index + 1}. {variant}")
-    player_choice_index = input()
-    try:
-        player_choice_index = int(player_choice_index)
-    except ValueError:
-        print("Вы ввели неправильный вариант. Попробуйте еще раз.")
-        continue
-
-    # Проверка выбора игрока
-    if player_choice_index not in range(1, len(variants) + 1):
-        print("Вы ввели неправильный вариант. Попробуйте еще раз.")
-        continue
-    player_choice = variants[player_choice_index - 1]
-
-    # Выбор компьютера
-    computer_choice = random.choice(variants)
-
-    # Вывод выбора игрока и компьютера
-    print("Вы выбрали:", player_choice)
-    print("Компьютер выбрал:", computer_choice)
-
-    # Определение победителя
-    if player_choice == computer_choice:
-        print("Ничья!")
-    elif (player_choice == 'камень' and computer_choice == 'ножницы') \
-        or (player_choice == 'ножницы' and computer_choice == 'бумага') \
-        or (player_choice == 'бумага' and computer_choice == 'камень'):
-        print("Вы победили!")
-    else:
-        print("Компьютер победил!")
-
-    # Запрос на повтор игры
+def inp(): 
+    global n
     while True:
-        print("Хотите сыграть еще раз?:\n1.да\n2.нет")
         try:
-            play_again = int(input())
-            if play_again == 2:
-                end_game = False
-                break
-            elif play_again == 1:
-                print("Игра продолжается..")
-                break
-            else:
-                print("Выберите вариант из представленных")
-                continue
+            n = int(input('Введите количество игр: '))
+            return n
         except ValueError:
-            print("Вы ввели неправильный вариант. Попробуйте еще раз.")
-            continue
-    
-print("Спасибо за игру!")
+            print('Это не число!')
+
+def logic(x, y): 
+    global CountD, CountW, CountL
+    if x == y:
+        print('Ничья')
+        CountD += 1
+    elif (x == 'k' and y == 'n') or (x == 'n' and y == 'b') or (x == 'b' and y == 'k'):
+        print('Выигрыш')
+        CountW += 1
+    else:
+        print('Проигрыш')
+        CountL += 1      
+    return CountD, CountL, CountW
+
+inp()
+while n != 0:
+    while True:
+        x = input('Камень (k), Ножницы (n), Бумага (b) -> ')
+        if x not in l:
+            print('Введите k, n, b!')
+        else: break
+
+    y = random.choice(l)
+    logic(x, y)
+    n -= 1
+else:
+    print('Победы - {0}\nПроигрыши - {1}\nНичьи- {2}'.format(CountW, CountL, CountD))
