5814f5d685597bd9b651a1835f53bff0c079f115
сырочек ✨ <57293178+sunsushishine@users.noreply.github.com>
2023-05-02 14:06:59 +0300

    Update 1_1 (2).py

--- Задание 1_1/1_1 (2).py
+++ Задание 1_1/1_1 (2).py
@@ -1,10 +1,10 @@
-m=0
+m = 0
 for i in range(100):
     x = input('Введите целое число или Стоп -> ')
     try:
-        m=m+int(x)
+        m = m + int(x)
     except ValueError:
-        if(type(x) is str and x=='Стоп'):
+        if(type(x) is str and x == 'Стоп'):
                 print('Сумма чисел равна',m)
                 break
         else:
